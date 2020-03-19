ee7da06d0230939a21bc9abda1466c9b6bc943a7
Henry <hsabio.a@gmail.com>
2020-03-18 21:43:43 -0400

    Refactored to loop through object keys to validate input instead of manually checking each input field

---
+++ js/formValidation.js
@@ -0,0 +1,76 @@
+const form = document.getElementById('form');
+const formData = {
+    username: document.getElementById('username'),
+    email: document.getElementById('email'),
+    password: document.getElementById('password'),
+    password2: document.getElementById('password2'),
+}
+
+//Show Input Error Message
+const showError = (input, message) => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small')
+    small.innerText = message;
+}
+
+const showSuccess = input => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Check for valid email
+function isValidEmail(email) {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regEx.test(String(email).toLowerCase());
+}
+
+function checkRequired(inputObj) {
+    const keys = Object.values(formData)
+    for (const input of keys) {
+        if(input.value.trim() === '') {
+            showError(input, ` ${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input);
+        }
+    }
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+//Event listeners
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    checkRequired(Object.keys(formData));
+
+    // **Long way - Gets messy if we have more fields to check.**
+
+    // if (formData.username.value === '') {
+    //     showError(username, "Username is required");
+    // } else {
+    //     showSuccess(username);
+    // }
+
+    // if (formData.email.value === '') {
+    //     showError(email, "Email Address is required");
+    // } else if (!isValidEmail(formData.email.value)) {
+    //     showError(email, "Email is not valid");
+    // } else {
+    //     showSuccess(email);
+    // }
+
+    // if (formData.password.value === '') {
+    //     showError(password, "Password is required");
+    // } else {
+    //     showSuccess(password);
+    // }
+
+    // if (formData.password2.value === '') {
+    //     showError(password2, "Please confirm password");
+    // } else {
+    //     showSuccess(password2);
+    // }
+})
